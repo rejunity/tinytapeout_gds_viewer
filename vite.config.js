a3c3d8b98e2b1f77e6d566524bfedbacb272f346
rej <rzioma@acm.org>
2024-04-24 11:16:00 +0200

    Run prettier

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,19 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   base: '', // this allows deployment to a subdirectory (e.g. https://tinytapeout.github.io/tinytapeout_gds_viewer/)
 
-  plugins: [{
+  plugins: [
+    {
       name: 'watch-gltf-files',
-      handleHotUpdate({ file, server }) { // this allows hot-reloading of .gltf files
+      handleHotUpdate({ file, server }) {
+        // this allows hot-reloading of .gltf files
         if (file.endsWith('.gltf')) {
           server.ws.send({
             type: 'custom',
-            event: 'my-gltf-change',      // viewer.js listens for this event
-            data: {}
+            event: 'my-gltf-change', // viewer.js listens for this event
+            data: {},
           });
         }
-      }
-    }],
+      },
+    },
+  ],
 });
